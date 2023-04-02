82f0bf112ae74ab7c7fbe8f3a59ce93584a5e07e
fabiovasques87 <fabioportovasques@yahoo.com.br>
2023-04-02 01:38:37 -0500

    identado o codigo da rota consultaVeiculo

--- src/views/base/consulta/veiculo/ConsultaVeiculo.js
+++ src/views/base/consulta/veiculo/ConsultaVeiculo.js
@@ -84,147 +84,141 @@ const ConsultaVeiculo = () => {
                     <h2 className="mb-5">Consultar veículos</h2>
              
                    <CForm onSubmit={handleSubmit()}  name='cad_veiculo'> 
-                     <CRow className="mb-8">
-                      
-
-                     <CFormLabel htmlFor="placaVeiculo" className="col-sm-2 col-form-label">Placa do Veículo <span>*</span></CFormLabel>
-                          <CCol sm={4}>
-                            <CFormInput type="text" name='placaVeiculo' id="placaVeiculo"                          
-                            {...register("placaVeiculo", {
-                              required: "Preenchimento obrigatório",
-                              minLength: {
-                                value: 7,
-                                message: "Mínimo 7 Dígitos"
-                              }
-                            })}                                                    
-                            />{errors.placaVeiculo && <span className='errors-alert'>{errors.placaVeiculo.message}</span>}
-                          </CCol>
+                    <CRow className="mb-8">
+                
+                        <CFormLabel htmlFor="placaVeiculo" className="col-sm-2 col-form-label">Placa do Veículo <span>*</span></CFormLabel>
+                            <CCol sm={4}>
+                                <CFormInput type="text" name='placaVeiculo' id="placaVeiculo"                          
+                                {...register("placaVeiculo", {
+                                required: "Preenchimento obrigatório",
+                                minLength: {
+                                    value: 7,
+                                    message: "Mínimo 7 Dígitos"
+                                }
+                                })}                                                    
+                                />{errors.placaVeiculo && <span className='errors-alert'>{errors.placaVeiculo.message}</span>}
+                            </CCol>
 
                           <CFormLabel htmlFor="tipo_veiculo" className="col-sm-2 col-form-label">Tipo Veículo <span>*</span></CFormLabel>
-                            <CCol sm={4}>
-                              <CFormSelect aria-label="Default select example" id='tipo_veiculo' value={buscaVeiculo} name='tipo_veiculo' onChange={e=>setBuscaVeiculo(e.target.value)}                            
-                                required
-                              > 
-                                
-                                <option>Selecione uma opção</option>
-                                <option value="carros">Carro</option>
-                                <option value="motos">Moto</option>
-                                <option value="caminhoes">Caminhão</option>
+                                <CCol sm={4}>
+                                <CFormSelect aria-label="Default select example" id='tipo_veiculo' value={buscaVeiculo} name='tipo_veiculo' onChange={e=>setBuscaVeiculo(e.target.value)}                            
+                                    required
+                                > 
+                                    
+                                    <option>Selecione uma opção</option>
+                                    <option value="carros">Carro</option>
+                                    <option value="motos">Moto</option>
+                                    <option value="caminhoes">Caminhão</option>
 
-                              </CFormSelect>                         
-                               {/*   <p>o tipo do veiculo selecionado foi: {buscaVeiculo}</p>    */}                                   
-                                {errors.tipo_veiculo && <span className='errors-alert'>{errors.tipo_veiculo.message}</span>}
+                                </CFormSelect>                         
+                                {/*   <p>o tipo do veiculo selecionado foi: {buscaVeiculo}</p>    */}                                   
+                                    {errors.tipo_veiculo && <span className='errors-alert'>{errors.tipo_veiculo.message}</span>}
+                                </CCol>
+
+                     </CRow>
+
+                    <CRow className="mb-8">
+
+                        <CFormLabel htmlFor="marca_veiculo" className="col-sm-2 col-form-label">Fabricante<span>*</span></CFormLabel>
+                            <CCol sm={4}>
+                                <CFormSelect name='fabrica_veiculo' id='options'                           
+                                > 
+                                <option value={buscaVeiculos()}>Selecionae uma opção</option> {/*vai buscar os dados da API  */}
+                                    {options.map(option=>(
+                                    //busca os dados da api conforme busca no campo tippo do veiculo
+                                    <option key={option.codigo} value={option.codigo}>{option.nome}</option>
+                                    
+                                ))}
+                                
+                                
+                                {/* {alert(resultBuscaveiculo)} */}
+
+                                </CFormSelect>                                                     
+                                {errors.marca_veiculo && <span className='errors-alert'>{errors.marca_veiculo.message}</span>}
+                            </CCol>
+
+                            <CFormLabel htmlFor="modelo_veiculo" className="col-sm-2 col-form-label">Marca Veiculo <span>*</span></CFormLabel>
+                                <CCol sm={4}>
+                                <CFormInput type='text' name='modelo_veiculo' id='modelo_veiculo'
+                                {...register("modelo_veiculo", {
+                                required: "Preenchimento obrigatório",
+                                minLength: {
+                                    value: 3,
+                                    message: "Mínimo 3 Dígitos"
+                                }
+                                })}    
+                                />                                                                                                      
+                                {errors.modelo_veiculo && <span className='errors-alert'>{errors.modelo_veiculo.message}</span>}
+                                </CCol>
+
+                    </CRow>
+
+                    <CRow className="mb-8">
+
+                      <CFormLabel htmlFor="ano_fabricacao" className="col-sm-2 col-form-label">Ano de Fabricação</CFormLabel>
+                            <CCol sm={4}>
+                            <CFormInput type="number" id="ano_fabricacao" name='ano_fabricacao' />                        
+                            </CCol>
+
+                            <CFormLabel htmlFor="cor_veiculo" className="col-sm-2 col-form-label">Cor do Veículo</CFormLabel>
+                                <CCol sm={4}>
+                                <CFormInput id='cor_veiculo' name='cor_veiculo'/>
+                                                                                    
+                                {errors.marca_veiculo && <span className='errors-alert'>{errors.marca_veiculo.message}</span>}
+                                </CCol>
+                                </CRow>
+                            <hr />
+                    <CRow className="mb-8">
+
+                          <CFormLabel htmlFor="proprietario" className="col-sm-2 col-form-label">CPF<span>*</span></CFormLabel>
+                            <CCol sm={4}>
+                            <CFormInput type="text" name='cpf_cnpj' id="cpf_cnpj" onChange={handleChangeMask} value={valor}                        
+                            required
+                            /> 
+                            </CCol>
+
+                      <CFormLabel htmlFor="nomeProprietario" className="col-sm-2 col-form-label">Nome <span>*</span></CFormLabel>
+                            <CCol sm={4}>
+                            <CFormInput type="text" name='nomeProprietario' id="nomeProprietario"                          
+                            {...register("nomeProprietario", {
+                                required: "Preenchimento obrigatório",
+                                minLength: {
+                                value: 3,
+                                message: "Mínimo 6 caracteres"
+                                }
+                            })}                                                    
+                            />{errors.nomeProprietario && <span className='errors-alert'>{errors.nomeProprietario.message}</span>}
                             </CCol>
 
                     </CRow>
 
                     <CRow className="mb-8">
 
-                    <CFormLabel htmlFor="marca_veiculo" className="col-sm-2 col-form-label">Fabricante<span>*</span></CFormLabel>
+                        <CFormLabel htmlFor="sobrenome" className="col-sm-2 col-form-label">Sobrenome <span>*</span></CFormLabel>
                             <CCol sm={4}>
-                            <CFormSelect name='fabrica_veiculo' id='options'                           
-                            > 
-                              <option value={buscaVeiculos()}>Selecionae uma opção</option> {/*vai buscar os dados da API  */}
-                                {options.map(option=>(
-                                  //busca os dados da api conforme busca no campo tippo do veiculo
-                                  <option key={option.codigo} value={option.codigo}>{option.nome}</option>
-                                   
-                              ))}
-                               
-                              
-                              {/* {alert(resultBuscaveiculo)} */}
-
-                            </CFormSelect>                                                     
-                             {errors.marca_veiculo && <span className='errors-alert'>{errors.marca_veiculo.message}</span>}
+                                <CFormInput type="text" name='sobrenome' id="sobrenome"                          
+                                {...register("sobrenome", {
+                                required: "Preenchimento obrigatório",
+                                minLength: {
+                                    value: 4,
+                                    message: "Mínimo 4  caracteres"
+                                }
+                                })}                                                    
+                                />{errors.sobrenome && <span className='errors-alert'>{errors.sobrenome.message}</span>}
                             </CCol>
-
-                            <CFormLabel htmlFor="modelo_veiculo" className="col-sm-2 col-form-label">Marca Veiculo <span>*</span></CFormLabel>
-                            <CCol sm={4}>
-                            <CFormInput type='text' name='modelo_veiculo' id='modelo_veiculo'
-                             {...register("modelo_veiculo", {
-                              required: "Preenchimento obrigatório",
-                              minLength: {
-                                value: 3,
-                                message: "Mínimo 3 Dígitos"
-                              }
-                            })}    
-                            />                                                                                                      
-                              {errors.modelo_veiculo && <span className='errors-alert'>{errors.modelo_veiculo.message}</span>}
-                            </CCol>
-
-                          </CRow>
-
-                      <CRow className="mb-8">
-
-                      <CFormLabel htmlFor="ano_fabricacao" className="col-sm-2 col-form-label">Ano de Fabricação</CFormLabel>
-                        <CCol sm={4}>
-                          <CFormInput type="number" id="ano_fabricacao" name='ano_fabricacao' />                        
-                        </CCol>
-
-                        <CFormLabel htmlFor="cor_veiculo" className="col-sm-2 col-form-label">Cor do Veículo</CFormLabel>
-                            <CCol sm={4}>
-                            <CFormInput id='cor_veiculo' name='cor_veiculo'/>
-                                                                                 
-                             {errors.marca_veiculo && <span className='errors-alert'>{errors.marca_veiculo.message}</span>}
-                            </CCol>
-                            </CRow>
-                        <hr />
-                        <CRow className="mb-8">
-
-                          <CFormLabel htmlFor="proprietario" className="col-sm-2 col-form-label">CPF<span>*</span></CFormLabel>
-                        <CCol sm={4}>
-                          <CFormInput type="text" name='cpf_cnpj' id="cpf_cnpj" onChange={handleChangeMask} value={valor}                        
-                          required
-                        /> 
-                        </CCol>
-
-                      <CFormLabel htmlFor="nomeProprietario" className="col-sm-2 col-form-label">Nome <span>*</span></CFormLabel>
-                        <CCol sm={4}>
-                          <CFormInput type="text" name='nomeProprietario' id="nomeProprietario"                          
-                          {...register("nomeProprietario", {
-                            required: "Preenchimento obrigatório",
-                            minLength: {
-                              value: 3,
-                              message: "Mínimo 6 caracteres"
-                            }
-                          })}                                                    
-                          />{errors.nomeProprietario && <span className='errors-alert'>{errors.nomeProprietario.message}</span>}
-                        </CCol>
-
-                          </CRow>
-
-                          <CRow className="mb-8">
-
-                      <CFormLabel htmlFor="sobrenome" className="col-sm-2 col-form-label">Sobrenome <span>*</span></CFormLabel>
-                          <CCol sm={4}>
-                            <CFormInput type="text" name='sobrenome' id="sobrenome"                          
-                            {...register("sobrenome", {
-                              required: "Preenchimento obrigatório",
-                              minLength: {
-                                value: 4,
-                                message: "Mínimo 4  caracteres"
-                              }
-                            })}                                                    
-                            />{errors.sobrenome && <span className='errors-alert'>{errors.sobrenome.message}</span>}
-                          </CCol>
 
                           <CFormLabel htmlFor="sobrenome" className="col-sm-2 col-form-label">Status <span>*</span></CFormLabel>
-                          <CCol sm={4}>
-                            <CFormSelect >
-                                <option>Selecione uma opão</option>
-                                <option>Ativo  </option>
-                                <option>Inativo  </option>
-                            </CFormSelect>
-                          </CCol>
+                            <CCol sm={4}>
+                                <CFormSelect >
+                                    <option>Selecione uma opão</option>
+                                    <option>Ativo  </option>
+                                    <option>Inativo  </option>
+                                </CFormSelect>
+                            </CCol>
 
-                        </CRow>
+                    </CRow>
 
-                    <CRow className="mb-8">
-                      
-
-                        </CRow>
-
-
+                   
                         <CRow className="mb-8">    
 
                             <CCol sm={4}>
@@ -236,7 +230,7 @@ const ConsultaVeiculo = () => {
                   
                     
                     
-                   </CForm>
+                        </CForm>
                     </FormArea>
                 </CCardBody>
               </CCard>
